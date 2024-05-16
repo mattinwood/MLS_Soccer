baa241586f2244a920d027cc9caa26d6b6ef510c
Matt Inwood <matt.inwood@gmail.com>
2024-05-16 18:49:24 -0500

    Many changes

--- sportmonk.py
+++ sportmonk.py
@@ -1,12 +1,13 @@
 import requests
 import json
 import configparser
-from typing import List, Dict
+from typing import List, Dict, NewType, Optional
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 TOKEN = config['SPORTSMONKS']['API_KEY']
 BASE_URL = f'https://api.sportmonks.com/v3/'
+ISODate = NewType('ISODate', str)
 
 
 def gen_url(endpoint: str,
@@ -32,7 +33,7 @@ def gen_url(endpoint: str,
         url += f'&include={";".join(includes)}'
     if pagination:
         url += f'&{pagination}'
-    return url
+    return url + '&timezone=America/Chicago'
 
 
 def paginated_results(url: str) -> dict:
@@ -71,7 +72,91 @@ def paginated_results(url: str) -> dict:
     }
 
 
-def get_player(search=None, includes=None):
+def name_to_id(var: int | str, endpoint: str):
+    """
+    Convert a name or an id to the corresponding id in the specified endpoint.
+
+    :param var: The name or id to convert. Can be an integer or a string.
+    :param endpoint: The endpoint to search for the name or id.
+    :return: The corresponding id of the name or id in the specified endpoint.
+    :raises ValueError: If the var parameter is not a valid search parameter.
+    """
+    if isinstance(var, int):
+        return var
+
+    if isinstance(var, str):
+        search_result = requests.get(gen_url(f'{endpoint}/search/{var}')).json()['data']
+        for i, value in enumerate([team for team in search_result]):
+            if endpoint == 'teams':
+                print(f"{i + 1}. {value['name']: <30} Last Played: {value['last_played_at']}")
+            else:
+                print(f"{i + 1}. {value['name']}")
+        while True:
+            try:
+                selected = int(input("\nPlease select a number from the list: "))
+                if 1 <= selected <= len(search_result):
+                    return search_result[selected-1]['id']
+                else:
+                    print("Invalid selection. Please select a number from the list.")
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+
+    raise ValueError('Not a valid search parameter')
+
+
+def get_fixtures(
+        fixture_id: Optional[int] = None,
+        date: Optional[ISODate] = None,
+        team: Optional[str | int] = None,
+        date_range_end: Optional[ISODate] = None,
+        vs_team: Optional[str | int] = None):
+    # TODO: Add Docstring
+
+    include = None  # Default Include for lists of games
+
+    if fixture_id is not None:
+        endpoint = f'fixtures/{fixture_id}'
+        include = ['lineups', 'events', 'statistics', 'timeline', 'lineups.details']
+    elif all(var is None for var in [date, team, date_range_end, vs_team]):
+        endpoint = 'fixtures'
+    elif all(var is None for var in [team, date_range_end, vs_team]) and date is not None:
+        endpoint = f'fixtures/date/{date}'
+    elif all(var is None for var in [team, vs_team]) and all([date, date_range_end]):
+        endpoint = f'fixtures/between/{date}/{date_range_end}'
+    elif vs_team is None and all([date, date_range_end, team]):
+        endpoint = f'fixtures/between/{date}/{date_range_end}/{name_to_id(team, "teams")}'
+    elif all(var is None for var in [date, date_range_end]) and all([team, vs_team]):
+        endpoint = (f'fixtures/head-to-head/'
+                    f'{name_to_id(team, "teams")}/'
+                    f'{name_to_id(vs_team, "teams")}')
+    else:
+        raise LookupError('Invalid combination of parameters:')
+    fixtures = requests.get(gen_url(endpoint, includes=include)).json()
+
+    return fixtures
+
+
+def fixture_statistics_lookups(data):
+    for stat in data['data']['statistics']:
+        stat['type'] = TYPES_LOOKUP[stat['type_id']]
+    return data
+
+
+def fixture_lineup_detail_lookups(data):
+    for player in data['data']['lineups']:
+        for stat in player['details']:
+            stat['type'] = TYPES_LOOKUP[stat['type_id']]
+    return data
+
+def fixture_lineup_lookups(data):
+    for player in data['data']['lineups']:
+        player['lineup_type'] = TYPES_LOOKUP[player['type_id']]
+        player['position'] = TYPES_LOOKUP[player['position_id']]
+    return data
+
+
+
+def get_player(search=None):
     if search is None:
         raise ValueError('Please enter a search condition')
     if type(search) is str:
@@ -81,7 +166,7 @@ def get_player(search=None, includes=None):
             raise LookupError('No result found for your search string')
         print(f'Returning first result: {player["data"][0]["name"]}')
         search = player['data'][0]['id']
-    return requests.get(gen_url(f'players/{search}', includes=includes))
+    return requests.get(gen_url(f'players/{search}'))
 
 
 def update_lookup(file: dict, file_nm: str):
@@ -128,11 +213,22 @@ def update_type_lookup():
 
 
 try:
-    types_lookup = read_lookup('types')
+    TYPES_LOOKUP = read_lookup('types')
 except FileNotFoundError:
     update_type_lookup()
-    types_lookup = read_lookup('types')
+    TYPES_LOOKUP = read_lookup('types')
 
 
 if __name__ == '__main__':
-    print(types_lookup)
+    """
+    In Place for testing various endpoints during development. 
+    """
+    # matchups = get_fixtures(team='fire', vs_team='st. louis')
+
+    one_game = get_fixtures(fixture_id=19051528)
+    one_game = fixture_statistics_lookups(one_game)
+    one_game = fixture_lineup_detail_lookups(one_game)
+    one_game = fixture_lineup_lookups(one_game)
+
+    # games = get_fixtures(date='2024-05-05')
+    # shaqiri = get_player('Shaqiri').json()
